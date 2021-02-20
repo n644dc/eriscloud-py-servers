dd9b3d4ea6f921974e0ee7f496d289643df1d662
n644dc <n644dc@gmail.com>
2021-02-20 06:24:13 +0000

    adding sqlite and otherstructures.

--- rainDropService/server.py
+++ rainDropService/server.py
@@ -1,50 +1,23 @@
 import sys
+import controller
+import uuid
 from logging.config import dictConfig
 from flask import Flask
 from flask import request
 from flask import jsonify
 
 server = Flask(__name__)
-
-class commRestDialog:
-    def __init__(self, dropId, reqType, reqBody, resType=None, resBody=None):
-        self.rainDropId   = dropId
-        self.requestType  = reqType
-        self.requestBody  = reqBody
-
-        self.responseType = resType
-        self.responseBody = resBody
-
-    def setResponse(self, response):
-        self.responseType = response[0]
-        self.responseBody = response[1]
-
-    @property
-    def serialize(self):
-        return {
-            'rainDropId': self.rainDropId,
-            'requestType': self.requestType,
-            'requestBody': self.requestBody,
-            'responseType': self.responseType,
-            'responseBody': self.responseBody
-        }
-
-    @property
-    def toString(self):
-        return str(self.serialize)
-
-#class clientHandler:
-#    def __init__(self):
-#        continue
+control = controller.RainDropController()
 
 @server.route('/raindrop/params/global', methods=['POST'])
 def getGlobalParams(self):
     return ""
 
 
-@server.route('/raindrop/sequence', methods=['POST'])
+@server.route('/raindrop/sequence', methods=['GET', 'POST'])
 def rainDropBus():
-    commDialog = commRestDialog(request.json["rainDropId"], request.json["requestType"], request.json["requestBody"])
+    restDialogId = str(uuid.uuid4())
+    commDialog = controller.createCommDialog(restDialogId, request.json["rainDropId"], request.json["requestType"], request.json["requestBody"])
     
     responseType = "REGISTRATION_RESPONSE"
     responseBody = "Rain drop registered successfully."
